f33ecb9a442a2fb5502904490dc9f70eb1ef59bd
Mateusz (@pingwin) <grabekm90@users.noreply.github.com>
2019-02-07 03:26:39 +0100

    init-user script

--- s/init-cluster.sh
+++ s/init-cluster.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -e
 TOOL=${1:-docker}
 
-$TOOL exec mongo-router init-users
+$TOOL exec mongo-router init-admin-users
 $TOOL exec mongo-router init-shared-cluster
+$TOOL exec mongo-router init-users
